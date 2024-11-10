c9ee5042d2c60edd23dffacc7ec2a4c543ef6c4d
Ingvar <ingvar.soloma@gmail.com>
2024-11-10 22:20:14 +0000

    Adding generator

--- example/calculate_commissions.php
+++ example/calculate_commissions.php
@@ -2,21 +2,18 @@
 
 require_once(__DIR__ . '/bootstrap.php');
 
-
 use IngvarSoloma\FeeMaster\Models\Operation;
 use IngvarSoloma\FeeMaster\Services\CommissionCalculator;
 use IngvarSoloma\FeeMaster\Utils\CurrencyConverter;
 
-function readOperationsFromCsv(string $filePath): array
+function readOperationsFromCsv(string $filePath): \Generator
 {
-    $operations = [];
     if (($handle = fopen($filePath, 'r')) !== false) {
         while (($data = fgetcsv($handle, 1000, ',')) !== false) {
-            $operations[] = new Operation($data[0], (int)$data[1], $data[2], $data[3], (float)$data[4], $data[5]);
+            yield new Operation($data[0], (int)$data[1], $data[2], $data[3], (float)$data[4], $data[5]);
         }
         fclose($handle);
     }
-    return $operations;
 }
 
 function main(string $csvFilePath): void
